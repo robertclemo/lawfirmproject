3f00dc7200927bbce95552c992b8204c38042684
woodmaster9000 <72224625+woodmaster9000@users.noreply.github.com>
2020-11-08 16:47:12 -0500

    Add files via upload

---
+++ services_table.py
@@ -0,0 +1,50 @@
+import mysql.connector
+import os
+from os import system
+from mysql.connector import connect
+
+conn = connect(
+    user = "admin",
+    password="buttbutt",
+    host= "projectdb.c1lco9rivug4.us-east-2.rds.amazonaws.com",
+    database = "LawfirmProject_DB"
+)
+#except mysql.connector.Error as err:
+   # print(‘Error connecting to DB’)
+
+def displayServices():
+    system('cls')
+    tableName = "services"
+
+    try:
+        cursor = conn.cursor()
+        cursor.execute('SELECT * FROM services')
+        for row in cursor:
+            print(f'''
+            Service..... {row[1]}
+            Description ..... {row[2]}
+            Price per hour..... {row[3]}
+            ''')
+        cursor.close()
+        conn.close()
+    except(Exception, mysql.connector.Error) as error:
+            print('Error while fetching data from MySQL', error)
+
+displayServices()
+
+
+# SQL COMMANDS
+
+
+# create table services(
+#     serv_name text not null,
+#     serv_desc text not null,
+#     serv_price int not null
+# )
+
+# INSERT INTO services (serv_name, serv_desc, serv_price) values 
+# ('Tax Attorney', 'Back taxes, Unified returns, Halt wage garnishments, Leins and Levies, IRS Compromises', 200 ),
+# ('Defence Attorney', 'Defense of individuals and companies charged with criminal activity.', 150),
+# ('Deivorce Attorney', 'Division of assets, debt among spouses, child custody, and legal separations', 450 ),
+# ('Prosecuting Attorney', 'Represent local, state, or federal governments in criminal court cases. In addition to trying cases,', 2000),
+# ('Bankruptcy',  'Assist clients through court proceedings to reduce or eliminate debt or to proceed forward with bankruptcy.', 500)
