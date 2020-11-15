68c3705bd6032e38154271f82f7d30fddf82c38c
robertclemo <72224419+robertclemo@users.noreply.github.com>
2020-11-15 16:46:07 -0500

    Update main.py

--- main.py
+++ main.py
@@ -10,16 +10,16 @@ load_dotenv()
 
 
 conn = connect(
-    host = getenv('DB_URL2'),
-    user = getenv('DB_USER2'),
-    password = getenv('DB_PASSWORD2') ,
-    database =  getenv('DB_NAME2'),
-    #port = getenv('DB_PORT2')    
+    host = "127.0.0.1",
+    user = "root",
+    password = "12345",
+    database =  "nci_db",
+      
 )
 
-def displayStaff():
+def displayEmployees():
     system('cls')
-    tableName = "staff"
+    tableName = "employees"
     try:
         mycursor = conn.cursor()
         mycursor.execute(
@@ -28,41 +28,43 @@ def displayStaff():
         #rows = mycursor.fetchall()
         print(Style.RESET_ALL)
         #if(rows):
-        print(Fore.CYAN, 'Staff Information: ')
+        print(Fore.CYAN, 'Employee Information: ')
         print(Style.RESET_ALL)
         for row in mycursor:
             print(f'''{Fore.RED}
             Employee ID:.....................{Style.RESET_ALL}{Fore.BLUE}{row[0]}{Fore.WHITE}
-            First Name:......................{Style.RESET_ALL}{Fore.BLUE}{row[1]}{Fore.WHITE}
-            Last Name:.......................{Style.RESET_ALL}{Fore.BLUE}{row[2]}{Fore.WHITE}
-            Email:...........................{Style.RESET_ALL}{Fore.BLUE}{row[3]}{Fore.WHITE}
-            Specialty:.......................{Style.RESET_ALL}{Fore.BLUE}{row[4]}{Fore.WHITE}
-            Department:......................{Style.RESET_ALL}{Fore.BLUE}{row[5]}{Fore.WHITE}
+            Last Name:.......................{Style.RESET_ALL}{Fore.BLUE}{row[1]}{Fore.WHITE}
+            First Name:......................{Style.RESET_ALL}{Fore.BLUE}{row[2]}{Fore.WHITE}
+            Title:...........................{Style.RESET_ALL}{Fore.BLUE}{row[3]}{Fore.WHITE}
+            Reports To:......................{Style.RESET_ALL}{Fore.BLUE}{row[4]}{Fore.WHITE}
+            Email:...........................{Style.RESET_ALL}{Fore.BLUE}{row[5]}{Fore.WHITE}
+            Username:........................{Style.RESET_ALL}{Fore.BLUE}{row[6]}{Fore.WHITE}
+            Password:........................{Style.RESET_ALL}{Fore.BLUE}{row[7]}{Fore.WHITE}
             ''')
         mycursor.close()
         conn.close()
     except(Exception, mysql.connector.Error) as error:
         print('Error while fetching data from MySQL', error)
     
-displayStaff()
+displayEmployees()
 
-def insertStaffTable():
-    tableName = "staff"
-    count = int(input("How many staff members do you wish to add? >> "))
+def insertEmployeeTable():
+    tableName = "employees"
+    count = int(input("How many employees do you wish to add? >> "))
     cursor = conn.cursor()
     for i in range(count):
-        print(f'Please fill out the following for the {i+1} staff member: >> ')
+        print(f'Please fill out the following for the {i+1} employees: >> ')
         fname = str(input("Staff First Name >> "))
         lname = str(input("Staff Last Name >> "))
         email = str(input("Staff Email Address >> "))
         specialty = str(input("Staff Specialty >> "))
         department = str(input("Staff Department >> "))
         cursor.execute(
-            f"INSERT INTO staff (fname, lname, email, specialty, department) values ('{fname}', '{lname}', '{email}', '{specialty}', '{department}')")
+            f"INSERT INTO employees (Last_Name, First_Name, title, ReportsTo, email, username, Password) values ('{Last_Name}', '{First_Name}', '{Title}', '{ReportsTo}', '{Email}', '{username}', '{Password}')")
         conn.commit()
         print(f'Excellent! {fname} has been added to your table!')
         cursor = conn.cursor()
         print('Displaying your table')
-        displayStaff()
+        displayEmployees()
 
-insertStaffTable()
+insertEmployeeTable()
